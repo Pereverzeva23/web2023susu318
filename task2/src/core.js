1c8bdece05ec031f5972daa05c473078146c3feb
Pereverzeva23 <146169412+Pereverzeva23@users.noreply.github.com>
2023-11-26 17:10:41 +0500

    Update core.js

--- task2/src/core.js
+++ task2/src/core.js
@@ -1,80 +1,147 @@
-//Напишите функцию, которая проверяет, является ли число целым используя побитовые операторы
-function isInteger(n) {}
+// Проверяем, является ли число целым используя побитовые операторы
+function isInteger(n) {
+return (n ^ 0) === n;
+}
 
-//Напишите функцию, которая возвращает массив четных чисел от 2 до 20 включительно
-function even() {}
+// Возвращаем массив четных чисел от 2 до 20 включительно
+function even() {
+var arr =[];
+for (var i = 2; i <= 20; i += 2) {
+arr.push(i);
+}
+return arr;
+}
 
-//Напишите функцию, считающую сумму чисел до заданного используя цикл
-function sumTo(n) {}
+// Считаем сумму чисел до заданного используя цикл
+function sumTo(n) {
+var sum = 0;
+for (var i = 1; i <= n; i++) {
+sum += i;
+}
+return sum;
+}
 
-//Напишите функцию, считающую сумму чисел до заданного используя рекурсию
-function recSumTo(n) {}
+// Считаем сумму чисел до заданного используя рекурсию
+function recSumTo(n) {
+if (n === 1) {
+return 1;
+}
+return n + recSumTo(n - 1);
+}
 
-//Напишите функцию, считающую факториал заданного числа
-function factorial(n) {}
+// Считаем факториал заданного числа
+function factorial(n) {
+if (n === 0) {
+return 1;
+}
+return n * factorial(n - 1);
+}
 
-//Напишите функцию, которая определяет, является ли число двойкой, возведенной в степень
-function isBinary(n) {}
+// Определяем, является ли число двойкой, возведенной в степень
+function isBinary(n) {
+if (n === 0) {
+return false;
+}
+while (n !== 1) {
+if (n % 2 !== 0) {
+return false;
+}
+n = n / 2;
+}
+return true;
+}
 
-//Напишите функцию, которая находит N-е число Фибоначчи
-function fibonacci(n) {}
+// Находим N-е число Фибоначчи
+function fibonacci(n) {
+var fib = [0, 1];
+for (var i = 2; i <= n; i++) {
+fib[i] = fib[i - 1] + fib[i - 2];
+}
+return fib[n];
+}
 
-/** Напишите функцию, которая принимает начальное значение и функцию операции
- * и возвращает функцию - выполняющую эту операцию.
- * Если функция операции (operatorFn) не задана - по умолчанию всегда
- * возвращается начальное значение (initialValue)
- * @param initialValue
- * @param operatorFn - (storedValue, newValue) => {operation}
- * @example
- * const sumFn =  getOperationFn(10, (a,b) => a + b);
- * console.log(sumFn(5)) - 15
- * console.log(sumFn(3)) - 18
- */
-function getOperationFn(initialValue, operatorFn) {}
+// Создаем функцию операции, возвращающую результат операции или начальное значение
+function getOperationFn(initialValue, operatorFn) {
+return function (newValue) {
+if (operatorFn) {
+return operatorFn(initialValue, newValue);
+} else {
+return initialValue;
+}
+};
+}
 
-/**
- * Напишите функцию создания генератора арифметической последовательности.
- * При ее вызове, она возвращает новую функцию генератор - generator().
- * Каждый вызов функции генератора возвращает следующий элемент последовательности.
- * Если начальное значение не передано, то оно равно 0.
- * Если шаг не указан, то по дефолту он равен 1.
- * Генераторов можно создать сколько угодно - они все независимые.
- *
- * @param {number} start - число с которого начинается последовательность
- * @param {number} step  - число шаг последовательности
- * @example
- * const generator = sequence(5, 2);
- * console.log(generator()); // 5
- * console.log(generator()); // 7
- * console.log(generator()); // 9
- */
-function sequence(start, step) {}
+// Создаем генератор арифметической последовательности
+function sequence(start, step) {
+start = start || 0;
+step = step || 1;
+return function () {
+var result = start;
+start += step;
+return result;
+};
+}
 
-/**
- * Напишите функцию deepEqual, которая принимает два значения
- * и возвращает true только в том случае, если они имеют одинаковое значение
- * или являются объектами с одинаковыми свойствами,
- * значения которых также равны при сравнении с рекурсивным вызовом deepEqual.
- * Учитывать специфичные объекты(такие как Date, RegExp итп) не обязательно
- *
- * @param {object} firstObject - первый объект
- * @param {object} secondObject - второй объект
- * @returns {boolean} - true если объекты равны(по содержанию) иначе false
- * @example
- * deepEqual({arr: [22, 33], text: 'text'}, {arr: [22, 33], text: 'text'}) // true
- * deepEqual({arr: [22, 33], text: 'text'}, {arr: [22, 3], text: 'text2'}) // false
- */
-function deepEqual(firstObject, secondObject) {}
+// Сравниваем два объекта на равенство (по содержанию)
+function deepEqual(firstObject, secondObject) {
+if (firstObject === secondObject) {
+return true;
+}
+if (typeof firstObject !== "object" || typeof secondObject !== "object") {
+return false;
+}
+var keysFirst = Object.keys(firstObject);
+var keysSecond = Object.keys(secondObject);
+if (keysFirst.length !== keysSecond.length) {
+return false;
+}
+for (var key of keysFirst) {
+if (!keysSecond.includes(key)) {
+return false;
+}
+if (!deepEqual(firstObject[key], secondObject[key])) {
+return false;
+}
+}
+return true;
+}
 
 module.exports = {
-    isInteger,
-    even,
-    sumTo,
-    recSumTo,
-    factorial,
-    isBinary,
-    fibonacci,
-    getOperationFn,
-    sequence,
-    deepEqual,
+isInteger,
+even,
+sumTo,
+recSumTo,
+factorial,
+isBinary,
+fibonacci,
+getOperationFn,
+sequence,
+deepEqual,
 };
+console.log("Тест 1");
+console.log(isInteger(2));
+console.log("Тест 2");
+var arr = even();
+console.log(arr);
+console.log("Тест 3");
+console.log(sumTo(5));
+console.log("Тест 4");
+console.log(recSumTo(10));
+console.log("Тест 5");
+console.log(factorial(4));
+console.log("Тест 6");
+console.log(isBinary(13));
+console.log("Тест 7");
+console.log(fibonacci(6));
+console.log("Тест 8");
+var multiply = function(a,b) {return a-b;};
+var operation=getOperationFn(2, multiply);
+console.log(operation(7))
+console.log("Тест 9");
+var seq = sequence(1,2);
+console.log(seq());
+console.log(seq());
+console.log("Тест 10");
+var obj1 = {a: 1, b: 2};
+var obj2 = {a: 1, b: 2};
+console.log(deepEqual(obj1, obj2));
