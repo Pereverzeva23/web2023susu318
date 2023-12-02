cf6a769626f3285fef1fe78c37145bd6b9b928e8
Pereverzeva23 <146169412+Pereverzeva23@users.noreply.github.com>
2023-12-02 13:26:28 +0500

    Update oop.js

--- task3/src/oop.js
+++ task3/src/oop.js
@@ -1,33 +1,55 @@
-/**
- * Напишите класс геометрической точки, принимающей в конструкторе координаты X и Y
- * Если координаты не переданы - 0,0; Аналогично если только 1 координата.
- * Реализовать метод, который возвращает расстояние от точки до центра координат (0, 0)
- */
-class Point {}
-
-/**
- * Напишите класс геометрической точки в трехмерном пространстве (x, y, z),
- * который будет наследоваться от точки в двумерном пространстве.
- * Реализовать статический метод, который возвращает расстояние между Point3D.
- */
-class Point3D extends Point {
-    static vectorLength(a, b) {}
+class Point {
+constructor(x = 0, y = 0) {
+this.x = x;
+this.y = y;
 }
 
-/**
- * Напишите класс "очередь", в котором можно добавить элемент в конец и получить из начала.
- * Предусмотреть 2 варианта инициализации - массивом в конструкторе (из него создается очередь) и без параметров.
- * Для тех, кто доверяет, но проверяет: написать тесты на методы класса (oop.spec.js)
- */
+distanceToOrigin() {
+return Math.sqrt(this.x * this.x + this.y * this.y);
+}
+}
+class Point3D extends Point {
+constructor(x = 0, y = 0, z = 0) {
+super(x, y);
+this.z = z;
+}
+
+static vectorLength(a, b) {
+const dx = a.x - b.x;
+const dy = a.y - b.y;
+const dz = a.z - b.z;
+return Math.sqrt(dx * dx + dy * dy + dz * dz);
+}
+}
+
 class Queue {
-    push;
-    pop;
-    size;
-    clear;
+constructor(items = []) {
+this.items = items;
+}
+
+push(item) {
+this.items.push(item);
+}
+
+pop() {
+if (this.items.length > 0) {
+return this.items.shift();
+} else {
+return null;
+}
+}
+
+get size() {
+return this.items.length;
+}
+
+clear() {
+this.items = [];
+}
 }
 
 module.exports = {
-    Point,
-    Point3D,
-    Queue,
+Point,
+Point3D,
+Queue,
 };
